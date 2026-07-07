e2e1a1d01a9df6ddc0c5b0d746d8dfd32aa4b208
Miku Jr. <axfootballcards@gmail.com>
2026-07-07 20:25:28 +0200

    Initial commit

---
+++ service.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+
+resetprop ro.product.brand samsung
+resetprop ro.product.manufacturer Samsung
+resetprop ro.product.model "Galaxy S25 Ultra"
+resetprop ro.product.device tokay
+resetprop ro.build.product tokay
